35ffe28eaa85b940a7a71a7053853cbc085cb7b8
Hamidou Diallo <dio.hamidou@gmail.com>
2024-12-25 19:43:39 -0500

    Add additional methods to insert/delete at a specified position

--- data_structures/linear/singly_linked_list.py
+++ data_structures/linear/singly_linked_list.py
@@ -1,24 +1,48 @@
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
     def __str__(self):
         return str(self.data)
 
 
 class SinglyLinkedList:
-    def __init__(self, head=None):
-        self.head = head
+    def __init__(self):
+        self.head = None
+        self.size = 0
 
     def add_at_head(self, value):
         new_node = Node(value)
         if not self.head:
             self.head = Node(value)
+            self.size += 1
             return
 
         new_node.next = self.head
         self.head = new_node
+        self.size += 1
+
+    def add(self, value, pos):
+        new_node = Node(value)
+
+        if not self.head:
+            self.add_at_head(value)
+            return
+
+        current = self.head
+        curr_pos = 1
+
+        while current:
+            if pos - 1 == curr_pos:
+                break
+            curr_pos += 1
+            current = current.next
+
+        next_node = current.next
+        current.next = new_node
+        new_node.next = next_node
+        self.size += 1
 
     def delete_at_head(self):
         if not self.head:
@@ -27,6 +51,26 @@ class SinglyLinkedList:
         next_node = self.head.next
         deleted_node = self.head.data
         self.head = next_node
+        self.size -= 1
+        return deleted_node
+
+    def delete(self, pos):
+        if not self.head:
+            return -1
+
+        current, curr_pos = self.head, 1
+
+        while current.next:
+            if pos - 1 == curr_pos:
+                break
+            curr_pos += 1
+            current = current.next
+
+        deleted_node = current.next.data
+        next_node = current.next.next
+        current.next = next_node
+
+        self.size -= 1
         return deleted_node
 
     def search(self, value):
@@ -62,9 +106,7 @@ if __name__ == "__main__":
     node = sll.delete_at_head()
     node1 = sll.delete_at_head()
     node2 = sll.delete_at_head()
-
     print(node, node1, node2)
-
     sll.print_list()
 
     sll.add_at_head(91)
@@ -72,8 +114,14 @@ if __name__ == "__main__":
     sll.add_at_head(34)
     sll.add_at_head(29)
     sll.add_at_head(97)
-
     sll.print_list()
 
     print(sll.search(34))  # True
     print(sll.search(44))  # False
+
+    sll.add(16, 5)
+    sll.add(22, 7)
+    sll.print_list()
+
+    print(sll.delete(3))  # 34
+    sll.print_list()

186bb1e8b54b7227f224707115f0b31a21e65dc1
Hamidou Diallo <dio.hamidou@gmail.com>
2024-12-25 19:13:03 -0500

    Update UnionFind class

--- data_structures/non_linear/graph/union_find.py
+++ data_structures/non_linear/graph/union_find.py
@@ -1,51 +1,46 @@
-def make_set(vertex_count):
-	return [x for x in range(vertex_count)]
-
-
 class UnionFind:
-	def __init__(self, vertex_count):
-		self.parent = make_set(vertex_count)
-		self.rank = [1] * vertex_count
-		self.count = vertex_count
+    def __init__(self, size: int):
+        self.parent = self.make_set(size)
+        self.rank = [1] * size
+        self.count = size
 
-	def union(self, x, y):
-		root1 = self.find(x)
-		root2 = self.find(y)
+    def make_set(self, vertex_count: int):
+        return [x for x in range(vertex_count)]
 
-		if root1 == root2:
-			return
-		elif self.rank[root1] > self.rank[root2]:
-			self.parent[root2] = self.parent[root1]
-			self.rank[root1] += 1
-		else:
-			self.parent[root1] = self.parent[root2]
-			self.rank[root2] += 1
-		self.count -= 1
+    def find(self, x: int):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
 
-	def find(self, x):
-		if x != self.parent[x]:
-			self.parent[x] = self.find(self.parent[x])
-		return self.parent[x]
+        return self.parent[x]
+
+    def union(self, x: int, y: int):
+        vertex1 = self.find(x)
+        vertex2 = self.find(y)
+
+        if vertex1 == vertex2:
+            return
+
+        if self.rank[vertex1] > self.rank[vertex2]:
+            self.parent[vertex2] = self.parent[vertex1]
+            self.rank[vertex1] += 1
+        else:
+            self.parent[vertex1] = self.parent[vertex2]
+            self.rank[vertex2] += 1
+
+        self.count -= 1
+
 
 def main():
-	edges = [
-		[0, 2],
-		[1, 4],
-		[1, 5],
-		[2, 3],
-		[2, 7],
-		[4, 8],
-		[5, 8]
-	]
+    edges = [[0, 2], [1, 4], [1, 5], [2, 3], [2, 7], [4, 8], [5, 8]]
 
-	number_of_edges = 9
-	union_find = UnionFind(number_of_edges)
+    number_of_edges = 9
+    union_find = UnionFind(number_of_edges)
 
-	for vertex1, vertex2 in edges:
-		union_find.union(vertex1, vertex2)
+    for vertex1, vertex2 in edges:
+        union_find.union(vertex1, vertex2)
 
-	print(f"Number of Connected Components is {union_find.count}.")
+    print(f"Number of Connected Components is {union_find.count}.")
+
 
 if __name__ == "__main__":
-	main()
-
+    main()
